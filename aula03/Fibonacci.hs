7da41adf172237531128e1009aa1c005727c7047
danielarjordao <danielarjordao@hotmail.com>
2026-01-06 16:38:38 +0000

    Add Fibonacci.hs

---
+++ aula03/Fibonacci.hs
@@ -0,0 +1,9 @@
+{-
+	Fazer a sequência de Fibonacci com recursividade em Haskell sem utilizar inteligência artificial.
+
+	fib 0 = 1
+	fib 1 = 1
+	fib n = fib (n-1) + fib (n-2)
+
+	Trazer 3 casos de testes.
+-}
